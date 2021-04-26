289845e41745c008fede6e20b0a1e3bca787fc84
David Weiland <weiland.david@icloud.com>
2021-04-26 11:48:32 +0200

    intégration "fermeture du formulaire"

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,13 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//fermeture du formulaire
+// close modal event
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+//validation des informations utilisateur
