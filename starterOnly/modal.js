f6c1171000c9bee20ac3da902aebaa035a9a4f28
David Weiland <weiland.david@icloud.com>
2021-05-05 20:03:20 +0200

    correction bug condition + limites ages

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,12 +15,14 @@ const form = document.querySelector("form");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll(".close");
 
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
 
 // close modal event
 closeBtn.forEach((btnClose) => btnClose.addEventListener("click", closeModal));
@@ -29,13 +31,13 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-//const btnSubmit = document.querySelector('.btn-submit');
 
 //validation des informations utilisateur
 const textControl = document.querySelectorAll(".text-control");
 for( var j = 0; j < textControl.length; j++){
   textControl[j].setAttribute("data-compteur",j);
 }
+
 
 //Validation radio location
 const locationGame = document.querySelectorAll("input[type=radio]");
@@ -52,27 +54,25 @@ for(var k = 0; k < locationGame.length; k++) {
     locationGame[iControlLoc].removeAttribute("checked");
 })};
 
+
 //Validation checkbox Conditions d'utilisation
 const condition = document.querySelectorAll("input[type=checkbox");
 const checkbox1 = document.querySelector("#checkbox1");
 checkbox1.setAttribute("checked",true);
-
-for(var l = 0; l < condition.length; l++) {
-  condition[l].setAttribute("data-compt-cond",l);
-  condition[l].addEventListener('change', function(eventCondition){
-    var iControlCond = eventCondition.target.getAttribute("data-compt-cond");
-    var checkedCondition = eventCondition.target.getAttribute("checked");
-    if (!checkedCondition){
-      condition[iControlCond].setAttribute("checked",true);
-      conditionValue = eventCondition.target.value;
-      condition[iControlCond].parentNode.removeAttribute("data-error")
-      condition[iControlCond].parentNode.setAttribute("data-error-visible",false);
-    } else {
-    condition[iControlCond].removeAttribute("checked");
-    conditionValue = "";
-    condition[iControlCond].parentNode.setAttribute("data-error-visible",true);
-    condition[iControlCond].parentNode.setAttribute("data-error","Veuillez accepter les conditions d'utilisation");
-}})};
+checkbox1.addEventListener('change', function(eventCondition){  
+var checkedCondition = checkbox1.getAttribute("checked");
+    
+if (!checkedCondition){
+  checkbox1.setAttribute("checked",true);
+  conditionValue = checkbox1.value;
+  checkbox1.parentNode.removeAttribute("data-error");
+  checkbox1.parentNode.setAttribute("data-error-visible",false);
+} else {
+  checkbox1.removeAttribute("checked");
+  conditionValue = "";
+  checkbox1.parentNode.setAttribute("data-error-visible",true);
+  checkbox1.parentNode.setAttribute("data-error","Veuillez accepter les conditions d'utilisation");
+}});
 
 var firstValue;
 var lastValue;
@@ -81,6 +81,7 @@ var birthdateValue;
 var quantityValue;
 var locationValue = location1.value;
 var conditionValue = checkbox1.value;
+
 
 //validation des entrées alphanumériques
 for (var i = 0; i < formData.length; i++) {
@@ -104,7 +105,22 @@ for (var i = 0; i < formData.length; i++) {
               emailValue = value;
               break;
             case "birthdate" :
-              birthdateValue = value;
+              const dateValue = value.split('-');
+              const year = dateValue[0];
+              const month = dateValue[1];
+              const day = dateValue[2];
+              const newDateValue = day + '/' + month + '/' + year;
+              if (year<=1922){
+                formData[iControl].setAttribute("data-error", "Les tournois sont ouverts jusqu'à 99 ans")
+                formData[iControl].setAttribute("data-error-visible",true);
+                birthdateValue = "";
+              } else if (year>=2014) {
+                formData[iControl].setAttribute("data-error", "Les tournois sont ouverts aux plus de 13 ans")
+                formData[iControl].setAttribute("data-error-visible",true);
+                birthdateValue = "";
+              } else {
+              birthdateValue = newDateValue;
+              }
               break;
             case "quantity" :
               quantityValue = value;
@@ -147,7 +163,7 @@ for (var i = 0; i < formData.length; i++) {
       testInput(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$/,value);
       break;
     case "birthdate" :
-      testInput(/^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)\d{2})$/,value);
+      testInput(/^\d{4}(-)\d{2}(-)\d{2}$/,value);
       break;
     case "quantity" :
       testInput(/[1-9]{1,2}/,value);
@@ -155,33 +171,34 @@ for (var i = 0; i < formData.length; i++) {
     default:;
 }})};
 
+
 //Validation champs non-vide et envoi formulaire
 function validate(){
   if (firstValue == null || firstValue == "" || firstValue == undefined){
     alert("Merci d'indiquer votre prénom");
     return false;
-    } else if (lastValue == null || lastValue == "" || lastValue == undefined){
-      alert("Merci d'indiquer votre nom");
-      return false;
-      } else if (emailValue == null || emailValue == "" || emailValue == undefined){
-        alert("Merci d'indiquer votre email");
-        return false;
-        } else if (birthdateValue == null || birthdateValue == "" || birthdateValue == undefined){
-          alert("Merci d'indiquer votre date de naissance");
-          return false;
-          } else if (quantityValue == null || quantityValue == "" || quantityValue == undefined){
-            alert("Merci d'indiquer le nombre de tournois auxquels vous avez participé");
-            return false;
-          } else if (locationValue == null || locationValue == "" || locationValue == undefined){
-            alert("Merci d'indiquer la ville dans laquelle s'est déroulé le(s) tournois");
-            return false;
-            } else if (conditionValue == null || conditionValue == "" || conditionValue == undefined){
-              alert("Merci d'accepter les conditions d'utilisation");
-              return false;
-              } else {
-            form.style.display = "none";
-            modalBody.innerHTML = "<p class='validation' style='height:640px; display:flex; flex-direction:column; justify-content:center;'>Merci !<br/><br/>Votre réservation a été envoyée</p><button class='btn-submit btn-close' value='Close'>Close</button>"
-            const closeBtnModal = document.querySelector(".btn-close");
-            closeBtnModal.addEventListener('click',closeModal);
-            return true;
+  } else if (lastValue == null || lastValue == "" || lastValue == undefined){
+    alert("Merci d'indiquer votre nom");
+    return false;
+  } else if (emailValue == null || emailValue == "" || emailValue == undefined){
+    alert("Merci d'indiquer votre email");
+    return false;
+  } else if (birthdateValue == null || birthdateValue == "" || birthdateValue == undefined){
+    alert("Merci d'indiquer votre date de naissance");
+    return false;
+  } else if (quantityValue == null || quantityValue == "" || quantityValue == undefined){
+    alert("Merci d'indiquer le nombre de tournois auxquels vous avez participé");
+    return false;
+  } else if (locationValue == null || locationValue == "" || locationValue == undefined){
+    alert("Merci d'indiquer la ville dans laquelle s'est déroulé le(s) tournois");
+    return false;
+  } else if (conditionValue == null || conditionValue == "" || conditionValue == undefined){
+    alert("Merci d'accepter les conditions d'utilisation");
+    return false;
+  } else {
+    form.style.display = "none";
+    modalBody.innerHTML = "<p class='validation' style='height:640px; display:flex; flex-direction:column; justify-content:center;'>Merci !<br/><br/>Votre réservation a été envoyée</p><button class='btn-submit btn-close' value='Close'>Close</button>"
+    const closeBtnModal = document.querySelector(".btn-close");
+    closeBtnModal.addEventListener('click',closeModal);
+    return true;
 }};
